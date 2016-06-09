9f04ae5a840160a718caa5f0dee56fc6e54f1f59
Brian Grant and Colin W Kingen <colin@>
2016-06-09 10:07:41 -0700

    add check for valid number input

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
+// BUSINESS LOGIC
+var isNumberValid = true;
+
+var validateNumber = function(num) {
+  if (num < 1 || num > 3999 || !num) {
+    isNumberValid = false;
+    return "That was not a valid input."
+  } else {
+    return "Here is your Roman Numeralated Number!"
+  };
+};
+
+
+
+
+
+// USER INTERFACE
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
 
-    var inputNumber = parseInt($("#inputNumber").val());
+    var inputNumber = parseInt($("input#inputNumber").val());
+    var validNumber = validateNumber(inputNumber);
+    $(".output").text(validNumber);
 
-    alert(inputNumber);
-
-    event.preventDefault()
+    event.preventDefault();
   });
 });
